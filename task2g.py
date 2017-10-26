bf79e7f29e204f63623912dcac33bb3eb34ffd8b
ronycleyagra <ronycley@gmail.com>
2017-10-26 09:23:13 -0300

    Commit task2.py gor git

--- task2g.py
+++ task2g.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt1
 import matplotlib.pyplot as plt2
 from bokeh.charts import Bar, output_file, show
 
-cnxn = pymysql.connect(host='localhost',database='ocs',user='root',password='')
+cnxn = pymysql.connect(host='*',database='*',user='*',password='*')
 sql1 = "SELECT sc.path as path , count(re.registration_id) as qtd FROM ocs.registrations re, ocs.sched_confs sc WHERE re.sched_conf_id = sc.sched_conf_id group by path ;"
 df1 = pd.read_sql(sql1, cnxn)
 sql2 = "SELECT sc.path as path , pa.paper_id FROM ocs.papers pa, ocs.sched_confs sc WHERE sc.sched_conf_id = pa.sched_conf_id ;"
